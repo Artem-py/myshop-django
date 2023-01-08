4d74544d7234e282a4dc8c4292889020427b9459
Artem Chervontsev <artyom.chervontsev@gmail.com>
2023-01-08 17:48:09 +0300

    Create a coupon system

--- payment/tasks.py
+++ payment/tasks.py
@@ -26,8 +26,7 @@ def payment_completed(order_id):
     html = render_to_string('orders/order/pdf.html', {'order': order})
     out = BytesIO()
     stylesheets = [weasyprint.CSS(settings.STATIC_ROOT / 'css/pdf.css')]
-    weasyprint.html(string=html).write_pdf(out,
-                                           stylesheets=stylesheets)
+    weasyprint.HTML(string=html).write_pdf(out, stylesheets=stylesheets)
     # attach PDF file
     email.attach(f'order_{order.id}.pdf',
                  out.getvalue(),
